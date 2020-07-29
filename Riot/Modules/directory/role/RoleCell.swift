e7b3ba1e87779d788b2705e0fe0f33bf05694006
Rafi Sadat <rafi.sadat@act.gov.au>
2020-07-29 16:39:58 +1000

    adding blank cell classes and viewcontroller.

---
+++ Riot/Modules/directory/role/RoleCell.swift
@@ -0,0 +1,24 @@
+//
+//  RoleCell.swift
+//  Riot
+//
+//  Created by Naurin Afrin on 29/7/20.
+//  Copyright © 2020 matrix.org. All rights reserved.
+//
+
+import UIKit
+
+class RoleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Riot/Modules/directory/role/RoleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoleViewController.swift
+//  Riot
+//
+//  Created by Naurin Afrin on 29/7/20.
+//  Copyright © 2020 matrix.org. All rights reserved.
+//
+
+import UIKit
+
+class RoleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

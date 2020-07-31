c0403bc48f602e3d71674ae39ec426114035214d
Rafi Sadat <rafi.sadat@act.gov.au>
2020-07-31 15:54:55 +1000

    blank viewcontroller added to directory

---
+++ Riot/Modules/directory/people/ActPeopleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ActPeopleViewController.swift
+//  Riot
+//
+//  Created by Naurin Afrin on 31/7/20.
+//  Copyright © 2020 matrix.org. All rights reserved.
+//
+
+import UIKit
+
+class ActPeopleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        AppDelegate.the()?.masterTabBarController.navigationItem.title = NSLocalizedString("title_people", tableName: "Vector", comment: "")
+        AppDelegate.the().masterTabBarController.tabBar.tintColor = ThemeService.shared().theme.tintColor
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Riot/Modules/directory/role/models/RoleModel.swift
@@ -0,0 +1,15 @@
+//
+//  RoleModel.swift
+//  Riot
+//
+//  Created by Naurin Afrin on 31/7/20.
+//  Copyright © 2020 matrix.org. All rights reserved.
+//
+
+import Foundation
+
+struct RoleModel {
+    var name: String
+    var description: String
+    var isExpanded: Bool
+}

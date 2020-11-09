0a3fb0fb001f1755608ef0efb7bddc843d6c8ce5
Joseph Fergusson <joseph.fergusson@outlook.com.au>
2020-11-09 23:46:05 +1100

    Initial work on RoomSelector

---
+++ Riot/Modules/Home/Alternate/RoomSelector.swift
@@ -0,0 +1,28 @@
+// 
+// Copyright 2020 New Vector Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+
+class RoomSelector: AlternateHomeViewController {
+    var callback: ((MXRoom)->Void)!
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let roomcell = tableView.cellForRow(at: indexPath) as? MXKRecentTableViewCell else { return }
+        //guard let cellData = roomcell.value(forKey: "roomCellData") as? MXKRecentCellDataStoring else { return }
+        let account = MXKAccountManager.shared()?.accountKnowingRoom(withRoomIdOrAlias: roomcell.roomTitle.text)
+        guard let room = account?.mxSession.room(withAlias: roomcell.roomTitle.text) else { return }
+        callback(room)
+    }
+}

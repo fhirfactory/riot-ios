ebf04ff0fd210a199a4f315179ec19e1e36503c3
Joseph Fergusson <joseph.fergusson@outlook.com.au>
2020-11-13 10:14:46 +1100

    Seemingly fixed duplicate timeline issue

--- Riot/Modules/Room/RoomViewController.m
+++ Riot/Modules/Room/RoomViewController.m
@@ -5410,7 +5410,8 @@
                         [RoomDataSource loadRoomDataSourceWithRoomId:room.roomId initialEventId:nil andMatrixSession:self.mainSession onComplete:^(id roomDataSource) {
                             MXKRoomDataSource *rds = roomDataSource;
                             [rds sendMessageWithContent:event.content success:^(NSString *eventId) {
-                                
+                                [rds invalidateBubblesCellDataCache]; //this appears to fix an issue whereby if a message was received at the same time that we forwarded a message, a room could end up having duplicate timelines. I don't know why this was a thing that could happen, and I don't really want to know, but this fix seems to mean we can send the message from this current room, rather than having to actually display the room we're forwarding to.
+                                [rds markAllAsRead]; //This is another gross thing we have to do. If we don't the system won't mark the attachment we just sent at having been read by us, so phantom notifications will begin to appear over time. Worse still, since the messages were sent by us, the read receipt counting system won't display the badge unless there's also a notification from someone else, so depending on how many attachments you've sent, you can end up with a situation where, from one message from someone else, the number of missed notifications on a room will jump from 0 (or no badge) to greater than 10.
                             } failure:^(NSError *error) {
                                 //currently, there's not really much we can probably do if there's an error. This is an incredibly unlikely situation though, as the message will typically be under a kilobyte, and even if it doesn't correctly send due to network issues, can usually be resent when reconnected.
                             }];
